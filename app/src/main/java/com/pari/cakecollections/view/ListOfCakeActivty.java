0da4b93579335a6dba5328761643f33d93333d42
Parimala Holla <pammiholla@gmail.com>
2019-06-28 23:23:46 +0100

    MainActivity name is refactored.

--- app/src/main/java/com/pari/cakecollections/view/ListOfCakeActivty.java
+++ app/src/main/java/com/pari/cakecollections/view/ListOfCakeActivty.java
@@ -10,6 +10,6 @@ public class ListOfCakeActivty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_cake);
     }
 }
