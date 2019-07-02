aefab8aee253439e9efaec9437bfac58f6a8a3a8
Parimala Holla <pammiholla@gmail.com>
2019-07-02 16:01:06 +0100

    pull to refresh feature is implemented for recyclerview

---
+++ app/src/main/java/com/pari/cakecollections/view/RecyclerItemClickListener.java
@@ -0,0 +1,8 @@
+package com.pari.cakecollections.view;
+
+import com.pari.cakecollections.model.CakeDetail;
+
+public interface RecyclerItemClickListener {
+    void onItemClick(CakeDetail cakeDetail);
+
+}
